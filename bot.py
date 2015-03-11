e1acc2a79f707b949bda9d4b63f03b538e64d633
Omar Saleem <ofsaleem@gmail.com>
2015-03-11 19:27:34 -0400

    Updated bot to be a bit more robust.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import time
 from praw.helpers import comment_stream
 
 r = praw.Reddit("sup son fixer by /u/TheShadowZero")
-r.login()
+r.login('SupSonBot', 'password')
 
 target_text = '¯\_(ツ)_/¯'
 response_text = '¯\\\\\_(ツ)_/¯ -- here, you dropped a \\\ '
@@ -14,9 +14,13 @@ subreddit = r.get_subreddit('dota2')
 
 processed = []
 while True:
-	for c in subreddit.get_comments():
-		if target_text.decode("utf-8") in c.body and c.id not in processed:
-			c.reply(response_text)
-			processed.append(c.id)
-	
-	time.sleep(300)
+	try:
+		for c in subreddit.get_comments():
+			if target_text.decode("utf-8") in c.body and c.id not in processed:
+				c.reply(response_text)
+				processed.append(c.id)
+	except urllib2.HTTPError, e:
+		if e.code in [429, 500, 502, 503, 504]:
+			time.sleep(60)
+			pass
+	time.sleep(180)
