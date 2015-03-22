33a231ee1ff6f5da940876c3bcf2758e34b0ba7e
EC2 Default User <ec2-user@ip-172-30-1-97.ec2.internal>
2015-03-22 02:16:58 +0000

    Oops, fixed the import

--- bot.py
+++ bot.py
@@ -2,9 +2,10 @@
 import praw
 import time
 from praw.helpers import comment_stream
+from urllib2 import HTTPError
 
 r = praw.Reddit("sup son fixer by /u/TheShadowZero")
-r.login('SupSonBot', 'password')
+r.login()
 
 target_text = '¯\_(ツ)_/¯'
 response_text = '¯\\\\\_(ツ)_/¯ -- here, you dropped a \\\ '
@@ -19,7 +20,7 @@ while True:
 			if target_text.decode("utf-8") in c.body and c.id not in processed:
 				c.reply(response_text)
 				processed.append(c.id)
-	except urllib2.HTTPError, e:
+	except HTTPError, e:
 		if e.code in [429, 500, 502, 503, 504]:
 			time.sleep(60)
 			pass
